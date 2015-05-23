2294d2a20b2e15cfb4588d8da00a1790868849bf
Martin Wiso <martin@wiso.cz>
2015-05-23 21:14:20 +0200

    Use maps for rich messages

--- src/sr_message.erl
+++ src/sr_message.erl
@@ -18,7 +18,7 @@
 
 %% Types
 -type field_title() :: atom() | title | value.
--type field()       :: {field_title(), binary()}.
+-type field()       :: {field_title(), binary()} | map().
 -type fields()      :: [field()].
 -export_type([fields/0]).
 
@@ -27,59 +27,56 @@
 %%%============================================================================
 -spec format(binary(), binary(), binary()) -> binary().
 format(Title, Message, Color) ->
-    to_json({[{<<"fallback">>, ?l2b(Message)},
-              {<<"title">>,    ?l2b(Title)},
-              {<<"text">>,     ?l2b(Message)},
-              {<<"color">>,    Color}
-             ]}).
+    to_json(#{<<"fallback">>  => Message,
+              <<"title">>     => Title,
+              <<"text">>      => Message,
+              <<"color">>     => Color}).
 
 -spec format(binary(), binary(), binary(), binary()) -> binary().
 format(Title, Message, Color, IconUrl) ->
-    to_json({[{<<"fallback">>, ?l2b(Message)},
-              {<<"title">>,    ?l2b(Title)},
-              {<<"text">>,     ?l2b(Message)},
-              {<<"icon_url">>, IconUrl},
-              {<<"color">>,    Color}
-             ]}).
+    to_json(#{<<"fallback">>  => Message,
+              <<"title">>     => Title,
+              <<"text">>      => Message,
+              <<"icon_url">>  => IconUrl,
+              <<"color">>     => Color}).
 
 -spec format(binary(), binary(), binary(), binary(), binary()) -> binary().
 format(Title, Message, Color, URL, IconUrl) ->
-    to_json({[{<<"fallback">>,  ?l2b(Message)},
-              {<<"title">>,     ?l2b(Title)},
-              {<<"text">>,      ?l2b(Message)},
-              {<<"icon_url">>,  IconUrl},
-              {<<"image_url">>, ?l2b(URL)},
-              {<<"color">>,     Color}
-             ]}).
+    to_json(#{<<"fallback">>  => Message,
+              <<"title">>     => Title,
+              <<"text">>      => Message,
+              <<"icon_url">>  => IconUrl,
+              <<"image_url">> => URL,
+              <<"color">>     => Color}).
 
 -spec format_table(binary(), binary(), fields(), binary()) -> binary().
 format_table(Title, Message, Fields, Color) ->
-    to_json({[{<<"fallback">>, ?l2b(Message)},
-              {<<"title">>,    ?l2b(Title)},
-              {<<"text">>,     ?l2b(Message)},
-              {<<"fields">>,   lists:map(fun format_field/1, Fields)},
-              {<<"color">>,    Color}
-             ]}).
+    to_json(#{<<"fallback">>  => Message,
+              <<"title">>     => Title,
+              <<"text">>      => Message,
+              <<"fields">>    => lists:map(fun format_field/1, Fields),
+              <<"color">>     => Color}).
 
 -spec format_table(binary(), binary(), fields(), binary(), binary()) -> binary().
 format_table(Title, Message, Fields, Color, IconUrl) ->
-    to_json({[{<<"fallback">>, ?l2b(Message)},
-              {<<"title">>,    ?l2b(Title)},
-              {<<"text">>,     ?l2b(Message)},
-              {<<"fields">>,   lists:map(fun format_field/1, Fields)},
-              {<<"icon_url">>, IconUrl},
-              {<<"color">>,    Color}
-             ]}).
+    to_json(#{<<"fallback">>  => Message,
+              <<"title">>     => Title,
+              <<"text">>      => Message,
+              <<"fields">>    => lists:map(fun format_field/1, Fields),
+              <<"icon_url">>  => IconUrl,
+              <<"color">>     => Color}).
 
 
 %%%============================================================================
 %%% Internal functions
 %%%============================================================================
+format_field(Field) when is_map(Field) ->
+    maps:put(<<"short">>, true, Field);
 format_field(Field) ->
-    {[{<<"title">>, proplists:get_value(title, Field)},
-      {<<"value">>, proplists:get_value(value, Field)},
-      {<<"short">>, true}
-     ]}.
+    #{<<"title">> => proplists:get_value(title, Field),
+      <<"value">> => proplists:get_value(value, Field),
+      <<"short">> => true
+     }.
 
 to_json(Struct) ->
     jiffy:encode([Struct]).
